2b63f2049620ba5d9d8a075b36b2eeb8af57a0ee
Ilja Goushcha <iljagou@yahoo.com>
2016-12-11 17:58:37 -0800

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name melissaSteeleApp
+ * @description
+ * # melissaSteeleApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('melissaSteeleApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name melissaSteeleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the melissaSteeleApp
+ */
+angular.module('melissaSteeleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
