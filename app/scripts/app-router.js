6bd807161cfd2148bd135841cc669f5701b29c53
Ilja Goushcha <iljagou@yahoo.com>
2017-01-01 21:58:15 -0800

    added ui router

---
+++ app/scripts/app-router.js
@@ -0,0 +1,39 @@
+/**
+ * Created by ig on 1/1/17.
+ */
+
+angular
+  .module('melissaSteeleApp').config(function($locationProvider, $stateProvider, $urlRouterProvider) {
+
+    // $locationProvider.html5Mode(true);
+
+    // $locationProvider
+    //   .html5Mode(false)
+    //   .hashPrefix('');
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '../views/home/home.html'
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: '../views/about/about.html'
+      })
+      .state('services', {
+        url: '/services',
+        templateUrl: '../views/services/services.html'
+      })
+      .state('testimonials', {
+        url: '/testimonials',
+        templateUrl: '../views/testimonials/testimonials.html'
+      });
+      // .state('melissa-steele', {
+      //   url: '/melissa-steele',
+      //   templateUrl: '../views/melissa-steele.html'
+      // });
+
+
+});
